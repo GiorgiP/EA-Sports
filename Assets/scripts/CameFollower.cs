d2baba029014d0eb1f733e699bfceb492f7081f0
206-23\Admin <gio.pkhakadze@gmail.com>
2017-04-20 20:52:36 +0400

    EA Sports

---
+++ Assets/scripts/CameFollower.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class CameFollower : MonoBehaviour {
+
+
+    public Transform target;
+    private Vector3 distanceFromTarget;
+
+
+
+	void Start () {
+
+        distanceFromTarget = transform.position - target.position;
+		
+	}
+	
+	
+	void Update () {
+
+        transform.position = target.position + distanceFromTarget;
+
+		
+	}
+}
---
+++ Assets/scripts/PlayerMover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+
+    Vector3 deltaPos;
+
+    public float speedMultiplier;
+    //public Transform player;
+    public Rigidbody playerBody;
+
+	void Start () {
+
+        deltaPos = Vector3.zero;
+		
+	}
+	
+	
+	void Update () {
+
+        deltaPos.z = Input.GetAxis("Vertical");  //gadaadgileba z gerdze
+        deltaPos.x = Input.GetAxis("Horizontal"); //gadaadgileba x gerdze
+
+        //playerBody.position = playerBody.position + deltaPos; //modzraobis kodi
+
+        // Input.GetAxis("Horizontal");
+
+        //player.position += deltaPos;
+
+        //transform.position =+ deltaPos;
+
+	}
+    private void FixedUpdate()
+    {
+        // playerBody.position = playerBody.position + deltaPos;
+
+        playerBody.AddForce(deltaPos *speedMultiplier);
+
+
+    }
+
+}
