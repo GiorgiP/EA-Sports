c21e7f3e7ed2bb9236d6aee43222117f4f5a1efb
206-23\Admin <gio.pkhakadze@gmail.com>
2017-05-02 20:53:23 +0400

    Coin VS BadCoin

---
+++ Assets/scripts/CoinCount.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCount : MonoBehaviour
+{
+
+    public int score = 0;
+    public Text scoreText;
+  //  public string coinTagString;
+  //  public string badCoinTagString;
+
+
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        Debug.Log("Collision" + col.gameObject.name);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Coin")
+        {
+            score = score + 1;
+        }
+
+        else if (other.tag == "BadCoin" && score > 0)
+
+        {
+            
+                score = score - 1;
+            
+        }
+        Debug.Log("Triggered " + score + " " + other.gameObject.name);
+
+        scoreText.text = "Score: " + score;
+
+        Destroy(other.gameObject);
+
+    }
+}
---
+++ Assets/scripts/CoinRotor.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class CoinRotor : MonoBehaviour {
+
+
+    public float rotationSpeed;
+    
+
+
+	void Update () {
+
+        transform.Rotate(Vector3.up, rotationSpeed);
+		
+	}
+}
