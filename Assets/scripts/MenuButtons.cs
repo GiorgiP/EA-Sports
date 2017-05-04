eea3e398a26ebb2a14bae7dc0198d7b601d5b26b
206-23\Admin <gio.pkhakadze@gmail.com>
2017-05-04 20:34:14 +0400

    04.05.2017

---
+++ Assets/scripts/MenuButtons.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class MenuButtons : MonoBehaviour
+{
+
+    int score;
+
+
+
+    public void RestartGame()
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+
+    }
+}
